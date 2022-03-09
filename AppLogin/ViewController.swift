7548eb475cadbc81360a6ee4d7922172c7e676a7
Vital <slyvitax@mail.ru>
2022-03-09 20:07:22 +0300

    Button and code for this

--- AppLogin/ViewController.swift
+++ AppLogin/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var PasswordTextRield: UITextField!
+    
+    let userName = "Admin"
+    let password = "admin"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func loginInButton() {
+    }
+    
+    @IBAction func forgotUserNameButton() {
+        showAlert(title: "Ooops!", message: "Your name is \(userName) :)")
+    }
+    
+    @IBAction func forgotPasswordButton() {
+        showAlert(title: "Ooops!", message: "Your password is \(password)")
+    }
 }
 
+// MARK: - Private Metod
+extension ViewController {
+    fileprivate func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
