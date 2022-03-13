0427bc4593e4a2f394f04e5324ab38c384db997c
Vital <slyvitax@mail.ru>
2022-03-13 11:28:05 +0300

    add code in module User

--- AboutMe/Models/User.swift
+++ AboutMe/Models/User.swift
@@ -5,3 +5,33 @@
 //  Created by Виталий Бобрик on 12.03.22.
 //
 
+struct UserLogin {
+    let login: String
+    let password: String
+    let users: [User]
+
+    static func getUser() -> [UserLogin] {
+        [
+            UserLogin( login: "admin",
+                       password: "admin",
+                       users: [
+                         User(name: "Vitaliy",
+                             surname: "Bobrik",
+                             age: 34,
+                             sity: "Minsk",
+                             interests: "football, volleyball")
+                        ]
+                      )
+        ]
+    }
+}
+
+struct User {
+    let name: String
+    let surname: String
+    let age: Int
+    let sity: String
+    let interests: String
+}
+
+
